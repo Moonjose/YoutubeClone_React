7f76b86506ebe5fe3f2dc98ca6b62abe61e65996
Jose Antonio <ajose9975@gmail.com>
2022-09-04 20:26:30 -0300

    Consuming youtube API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
